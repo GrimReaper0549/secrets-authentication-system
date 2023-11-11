012e322c0b44fc947d8a92d755dbd0b99a7a6c56
GrimReaper0549 <sankalp.ddn@gmail.com>
2023-11-12 02:33:12 +0530

    Level 1 Implemented

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
 //jshint esversion:6
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+const userSchema=new mongoose.Schema({
+    username: String,
+    password: String
+})
+
+const User=mongoose.model("User", userSchema)
+
+app.get("/", (req, res)=>{
+    res.render("home")
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login")
+})
+
+app.post("/login", async (req, res)=>{
+    const username=req.body.username
+    const foundUser=await User.findOne({username: username}).exec();
+    // console.log(foundUser);
+    if(foundUser){
+        if(foundUser.password===req.body.password){
+            res.render("secrets");
+        }
+        else{
+            res.redirect("/login");
+        }
+    }
+    else{
+        res.redirect("/login");
+    }
+})
+
+app.get("/register", (req, res)=>{
+    res.render("register")
+})
+
+app.post("/register", (req, res)=>{
+    const username=req.body.username
+    const password=req.body.password
+    const newUser=new User({
+        username: username,
+        password: password
+    })
+    newUser.save();
+    res.render("secrets");
+
+})
+
+app.get("/logout", (req, res)=>{
+    res.redirect("/");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started at Port ${port}`);
+})
