3c23202acbcc32fad5cb2589c796aeeb972adbdf
GrimReaper0549 <sankalp.ddn@gmail.com>
2023-11-12 05:56:33 +0530

    Level 4 Implemented

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import ejs from "ejs";
-import mongoose from 'mongoose';
+import mongoose, { sanitizeFilter } from 'mongoose';
 // import encrypt from 'mongoose-encryption'
-import md5 from "md5"
+// import md5 from "md5"
+import bcrypt from "bcrypt";
 
 
-
+const saltRounds=10;
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -40,12 +41,14 @@ app.post("/login", async (req, res)=>{
     const foundUser=await User.findOne({username: username});
     // console.log(foundUser);
     if(foundUser){
-        if(foundUser.password==md5(req.body.password)){
-            res.render("secrets");
-        }
-        else{
-            res.redirect("/login");
-        }
+        bcrypt.compare(req.body.password, foundUser.password, (err, result)=>{
+            if(result===true){
+                res.render("secrets");
+            }
+            else{
+                res.redirect("/login");
+            }
+        });
     }
     else{
         res.redirect("/login");
@@ -59,13 +62,21 @@ app.get("/register", (req, res)=>{
 app.post("/register", (req, res)=>{
     const username=req.body.username
     const password=req.body.password
-    const newUser=new User({
-        username: username,
-        password: md5(password)
-    })
-    newUser.save();
-    res.render("secrets");
-
+    bcrypt.hash(password, saltRounds, async(err, hash)=>{
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            const newUser=new User({
+                username: username,
+                password: hash
+            })
+            await newUser.save();
+            res.render("secrets");
+        }
+    
+    });
 })
 
 app.get("/logout", (req, res)=>{
